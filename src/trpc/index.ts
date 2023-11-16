13636cd8284faf4f23a91495d00128f2eb256aab
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-11-16 18:51:05 +0300

    Added Chat Page

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,11 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
+import { z } from "zod";
+
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
@@ -10,10 +13,61 @@ export const appRouter = router({
       throw new TRPCError({
         code: "UNAUTHORIZED",
       });
-    
+
     //  check if user is in database
-    return {success: true}
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      // crate user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+
+    return { success: true };
   }),
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    });
+  }),
+  deleteFile: privateProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({code: "NOT_FOUND"})
+
+      await db.file.delete({
+        where: {
+          id: input.id
+        }
+      })
+
+      return file
+    }),
 });
 // Export type router type signature,
 // NOT the router itself.
