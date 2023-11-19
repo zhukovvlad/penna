50052d992864a2c1fb125d62785ae47c516bb408
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-11-19 15:36:48 +0300

    Start of implementing PdfRenderer

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2. React-dropzone
 3. Uploadthing
 4. Shadcn-UI
+5. React-resize-detector
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -44,19 +44,19 @@ export const appRouter = router({
   }),
   getFile: privateProcedure
     .input(z.object({ key: z.string() }))
-    .mutation(async ({ctx, input}) => {
-      const {userId} = ctx
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
 
       const file = await db.file.findFirst({
         where: {
           key: input.key,
-          userId
-        }
-      })
+          userId,
+        },
+      });
 
-      if (!file) throw new TRPCError({code: "NOT_FOUND"})
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
 
-      return file
+      return file;
     }),
 
   deleteFile: privateProcedure
