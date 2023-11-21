e2845689da54ada97ae88c4e4df1e103c67ac8a2
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-11-21 23:56:07 +0300

    Started implementation of Chat

---
+++ src/components/chat/ChatWrapper.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Messages from "./Messages";
+import ChatInput from "./ChatInput";
+import { trpc } from "@/app/_trpc/client";
+
+interface ChatWrapperProps {
+  fileId: string;
+}
+
+const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
+  const {data, isLoading} = trpc.getFileUploadStatus.useQuery(
+    {
+      fileId,
+    },
+    {
+      refetchInterval: (data) => 
+        data?.status === "SUCCESS" || data?.status === "FAILED" ? false : 500,
+      
+    }
+  );
+
+  return (
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1 justify-between flex flex-col mb-28">
+        <Messages />
+      </div>
+      <ChatInput />
+    </div>
+  );
+};
+
+export default ChatWrapper;
