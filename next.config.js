c7e61e40445b82c5bbe22eba2ebc2c9598da7090
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-12-03 14:51:54 +0300

    Fixed problems in Navbar

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "lh3.googleusercontent.com" }],
+  },
   webpack: (config, { build, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
